b10b3d89e2cd62ec42786d83a47b4ebf91780ebb
petuch03 <81352831+petuch03@users.noreply.github.com>
2021-09-08 23:22:11 +0300

    cleaned up code + combined alert

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let X, Y, R;
+let ALERT = '';
 
 function validateX() {
     let xButtons = document.getElementsByName('xVal');
@@ -9,11 +10,11 @@ function validateX() {
             xCounter++;
     })
     if (xCounter > 1) {
-        alert("Choose only 1 X option");
+        ALERT += "\n" + "Choose only 1 X option;";
         return false;
     }
     if (xCounter === 0) {
-        alert("Choose any X option");
+        ALERT += "\n" + "Choose any X option;";
         return false;
     }
     X = document.querySelector('input[name="xVal"]:checked').value;
@@ -28,11 +29,11 @@ function validateR() {
             yCounter++;
     })
     if (yCounter > 1) {
-        alert("Choose only 1 R option");
+        ALERT += "\n" + "Choose only 1 R option;";
         return false;
     }
     if (yCounter === 0) {
-        alert("Choose any R option");
+        ALERT += "\n" + "Choose any R option;";
         return false;
     }
     R = document.querySelector('input[name="rVal"]:checked').value;
@@ -40,21 +41,21 @@ function validateR() {
 }
 
 function validateY() {
-    let r = document.getElementById("yValue");
-    if (r.value.trim() === "") {
-        alert("Y value must not be null");
+    let y = document.getElementById("yValue");
+    if (y.value.trim() === "") {
+        ALERT += "\n" + "Y value must not be null;";
         return false;
     }
-    r.value = r.value.replace(',', '.');
-    if (!(r.value && !isNaN(r.value))) {
-        alert("Y must be a number");
+    y.value = y.value.replace(',', '.');
+    if (!(y.value && !isNaN(y.value))) {
+        ALERT += "\n" + "Y must be a number;";
         return false;
     }
-    if (r.value < -5 || r.value > 5) {
-        alert("Y must be in interval: (-5; 5)!");
+    if (y.value <= -5 || y.value >= 5) {
+        ALERT += "\n" + "Y must be in the following interval: (-5; 5);";
         return false;
     }
-    Y = r.value.replace(",", ".");
+    Y = y.value.replace(",", ".");
     return true;
 }
 
@@ -64,7 +65,8 @@ function validateForm() {
 
 function cal() {
     if (!validateForm()) {
-        alert('cringe');
+        alert(ALERT);
+        ALERT = '';
         return;
     }
     if (validateForm()) {
@@ -74,22 +76,18 @@ function cal() {
             'r': R,
             'timezone': new Date().getTimezoneOffset()
         }).done(function (data) {
-            alert("response got");
             let arr = JSON.parse(data);
             arr.forEach( function (elem) {
-                alert("start  for each");
                 if (!elem.validate) { return; }
                 let newRow = '<tr>';
                 newRow += '<td>' + elem.x + '</td>';
                 newRow += '<td>' + elem.y + '</td>';
                 newRow += '<td>' + elem.r + '</td>';
                 newRow += '<td>' + (elem.isHit ? "yes" : "no") + '</td>';
-                newRow += '<td>' + elem.duration + '</td>';
+                newRow += '<td>' + elem.duration.toFixed(7) + '</td>';
                 newRow += '<td>' + elem.current + '</td>';
                 $('#tableWithResults tr:first').after(newRow);
-                alert("end for each");
             })
-            alert("response got after for each");
         }).fail(function (err) {
             alert(err)
         });
